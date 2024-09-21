5da2a3dd56a1d651c153c66c4dbbd1638f0ac9a6
Denis <smutok.denis@gmail.com>
2024-09-21 23:24:06 +0300

    задача 1 и 2

---
+++ 2 semester/problem set 1.5.py
@@ -0,0 +1,99 @@
+# sets=list(map(int,input().split()))
+# # sets=list(map(int,[3, 4, -7, 3, 1, 3, 1, -4, -2, -2]))
+# dicts={}
+# for i in range(len(sets)):
+# 	sums=0
+# 	for j in range(i,len(sets)):
+# 		sums+=sets[j]
+# 		if sums==0:
+# 			dicts[i]=(i,j)
+# print(dicts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
