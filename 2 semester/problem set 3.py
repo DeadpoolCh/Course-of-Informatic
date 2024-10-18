238539db105cb8a503484dc926c96ebc8db72f78
Denis <smutok.denis@gmail.com>
2024-10-18 11:59:52 +0300

    работа в классе

---
+++ 2 semester/problem set 3.py
@@ -0,0 +1,114 @@
+# def unique(string: str) -> int:
+# 	result = 0
+# 	symbols=[]
+# 	for chr in string:
+# 		if chr not in symbols: symbols.append(chr)
+# 	result=len(symbols)
+# 	result=len(set(string))
+# 	return result
+#
+# assert unique("aaaaaaaa") == 1
+# assert unique("abab") == 2
+# assert unique("abcd") == 4
+
+
+# def is_anagram(word1: str, word2: str) -> bool:
+# 	result = False
+# 	symword1=[]
+# 	dictsym={}
+# 	count=0
+# 	# for chr in word1:
+# 	# 	if chr not in symword1: symword1.append(chr)
+# 	# for chrs in word2:
+# 	# 	if chrs in symword1: count+=1
+# 	# if count==len(word2): result=True
+#
+# 	if set(word1)==set(word2): result=True
+# 	return result
+#
+# assert is_anagram("abcd", "acdb")
+# assert not is_anagram("abcde", "dl")
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+	result = 0
+	chrset=set()
+	# dicts={}
+	# for i in range(len(s)):
+	# 	dicts.setdefault(s[i],"")
+	# 	dicts[s[i]]+=str(i)
+	# print(dicts)
+
+	return result
+lengthOfLongestSubstring('abcabcbb')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
